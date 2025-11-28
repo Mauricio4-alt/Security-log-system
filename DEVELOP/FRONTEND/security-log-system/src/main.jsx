4cf972d14903d0d6b279fda92448950a945dc4ee
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2025-11-28 14:23:01 -0500

    chore:actualizando espacios

--- DEVELOP/FRONTEND/security-log-system/src/main.jsx
+++ DEVELOP/FRONTEND/security-log-system/src/main.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter,Routes,Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import './assets/styles/index.css'
+
 import MyAppNav from "./nav";
 import Login from "./login";
+import Ingreso from "./Ingreso";       // <-- IMPORTANTE
+import SecurityLayout from "./security-layout";
+
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
-    
+
     <Routes>
-      <Route path="/"  element={<Login/>}/>  
-      <Route path="/security"  >
-        <MyAppNav/>
-        <Route path="registro-ingreso"  element={<Ingreso/>}/>  
-    </Route>
+
+      {/* RUTA PRINCIPAL */}
+      <Route path="/" element={<Login />} />
+
+      {/* RUTA PADRE */}
+      <Route path="/security" element={<SecurityLayout/>}>
+        <Route path="registro-ingreso" element={<Ingreso />} />
+      </Route>
 
     </Routes>
-  </BrowserRouter>,
 
-      
-     
+  </BrowserRouter>
 );
