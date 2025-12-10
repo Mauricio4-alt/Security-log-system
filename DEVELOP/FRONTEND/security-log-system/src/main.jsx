5a0522cd751e5a819d71c178ef245b7840462749
Cristopher1178Col <millerosa09@gmail.com>
2025-12-09 21:06:46 -0500

    Fer

--- DEVELOP/FRONTEND/security-log-system/src/main.jsx
+++ DEVELOP/FRONTEND/security-log-system/src/main.jsx
@@ -9,6 +9,7 @@ import WelcomeMessage from "./welcomeMessageCelador";
 import Login from "./login";
 import Registrar from "./registroIngreso";       // <-- IMPORTANTE
 import './assets/styles/index.css';
+import WelcomeMessageAdmin from "./welcomeMessageAdmin";
 
 const root = document.getElementById("root");
 
@@ -21,6 +22,11 @@ ReactDOM.createRoot(root).render(
       <Route path="/security" element={<MyAppNav />} >
         <Route index element={<WelcomeMessage />} />
         <Route path="registro-ingreso" element={<Registrar />} /> 
+      </Route>
+
+      <Route path="/admin">
+        <Route index element={<WelcomeMessageAdmin/>}/>
+
       </Route>
 
     </Routes>
---
+++ DEVELOP/FRONTEND/security-log-system/src/welcomeMessageAdmin.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './assets/styles/index.css'
+
+export default function WelcomeMessageAdmin() {
+    return (
+        <div className="p-6 md:p-10 max-w-3xl mx-auto bg-white rounded-xl shadow-2xl border-2 border-indigo-600/50">
+            <div className='text-center'>
+                
+                <h2 className="text-3xl font-extrabold text-gray-900 mb-2">
+                    👋 Bienvenido Funcionario Administrativo
+                </h2>
+                
+                <p className="text-lg text-gray-600">
+                Tu rol es fundamental para el manejo de la accesibilidad al sistema y verifación de los equipos que ingresan al complejo
+                </p>
+                
+                <p className="text-base font-semibold text-indigo-700 mt-4">
+                    Utiliza el <strong>MENÚ</strong> para acceder a las funciones de gestion de cuentas y listado de registros de funcionarios. ¡Comencemos!
+                </p>
+            </div>
+        </div>
+    );
+}
